574ebcc5d81cbe81e6901cffa01cdbd42f2c17f3
Wang Xiaoqiang <xq21@vip.qq.com>
2017-02-23 00:35:58 +0800

    can add todo

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,68 +3,60 @@ require('styles/App.css');
 
 import React from 'react';
 
-// let yeomanImage = require('../images/yeoman.png');
+import TodoApp from './TodoApp.js';
 
 class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        {id: 1, text: 'create app', completed: true},
+        {id: 2, text: 'new todo item should support'},
+        {id: 3, text: 'do not repeat it.'}
+      ]
+    };
+
+    this.nextId = 4;
+  }
+
+  genId() {
+    return this.nextId++;
+  }
+
+  handleEditTodo(id, newTodo) {
+    let {todos} = this.state;
+
+    let todo = todos.find(i => i.id == id);
+    Object.assign(todo, newTodo);
+
+    this.setState({todos});
+  }
+
+  handleAddTodo(text) {
+    let {todos} = this.state;
+
+    todos.push(Object.assign({}, {text}, {id: this.genId()}));
+
+    this.setState({todos});
+  }
+
+  handleRemoveTodo(id) {
+    let {todos} = this.state;
+
+    todos.splice(todos.findIndex(i => i.id == id), 1);
+
+    this.setState({todos});
+  }
+
   render() {
     return (
-      <div>
-        <section className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <input className="new-todo" placeholder="What needs to be done?" autoFocus />
-          </header>
-          {/*This section should be hidden by default and shown when there are todos*/}
-          <section className="main">
-            <input className="toggle-all" type="checkbox" />
-            {/*<label for="toggle-all">Mark all as complete</label>*/}
-            <ul className="todo-list">
-              {/*These are here just to show the structure of the list items*/}
-              {/*List items should get the class `editing` when editing and `completed` when marked as completed*/}
-              <li className="completed">
-                <div className="view">
-                  <input className="toggle" type="checkbox" checked />
-                  <label>Taste JavaScript</label>
-                  <button className="destroy" />
-                </div>
-                <input className="edit" value="Create a TodoMVC template" />
-              </li>
-              <li>
-                <div className="view">
-                  <input className="toggle" type="checkbox" />
-                  <label>Buy a unicorn</label>
-                  <button className="destroy" />
-                </div>
-                <input className="edit" value="Rule the web" />
-              </li>
-            </ul>
-          </section>
-          {/*This footer should hidden by default and shown when there are todos*/}
-          <footer className="footer">
-            {/*This should be `0 items left` by default*/}
-            <span className="todo-count"><strong>0</strong> item left</span>
-            {/*Remove this if you don't implement routing*/}
-            <ul className="filters">
-              <li>
-                <a className="selected" href="#/">All</a>
-              </li>
-              <li>
-                <a href="#/active">Active</a>
-              </li>
-              <li>
-                <a href="#/completed">Completed</a>
-              </li>
-            </ul>
-            {/*Hidden if no completed items are left ↓*/}
-            <button className="clear-completed">Clear completed</button>
-          </footer>
-        </section>
-        <footer className="info">
-          <p>Double-click to edit a todo</p>
-          <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-        </footer>
-      </div>
-    );
+      <TodoApp
+        todos={this.state.todos}
+        onRemoveTodo={this.handleRemoveTodo.bind(this)}
+        onAddTodo={this.handleAddTodo.bind(this)}
+        onEditTodo={this.handleEditTodo.bind(this)}
+      />);
   }
 }
 
