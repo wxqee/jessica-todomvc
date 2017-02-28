3e2103ab51f4e5fb764c6d9029cbe5acbd52b20c
Grace Yang <grace.yang@otms.cn>
2017-02-28 23:55:08 +0800

    start my todo App

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,66 +3,87 @@ require('styles/App.css');
 
 import React from 'react';
 
-// let yeomanImage = require('../images/yeoman.png');
+let ENTER_KEY = 13;
 
 class AppComponent extends React.Component {
+
+  newTodo(e) {
+    if (e.keyCode == 13) {
+      alert();
+    }
+  }
+
+  renderHeader() {
+    return (
+      <header className="header">
+        <h1>todos</h1>
+        <input className="new-todo" placeholder="What needs to be done?" autoFocus onKeyDown={this.newTodo.bind(this)} />
+      </header>
+    );
+  }
+
+  renderMain() {
+    return (
+      <section className="main">
+        <input className="toggle-all" type="checkbox" />
+        {/*<label for="toggle-all">Mark all as complete</label>*/}
+        <ul className="todo-list">
+          <li className="completed">
+            <div className="view">
+              <input className="toggle" type="checkbox" checked />
+              <label>Taste JavaScript</label>
+              <button className="destroy" />
+            </div>
+            <input className="edit" value="Create a TodoMVC template" />
+          </li>
+          <li>
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>Buy a unicorn</label>
+              <button className="destroy" />
+            </div>
+            <input className="edit" value="Rule the web" />
+          </li>
+        </ul>
+      </section>
+    );
+  }
+
+  renderFooter() {
+    return (
+      <footer className="footer">
+        {/*This should be `0 items left` by default*/}
+        <span className="todo-count"><strong>0</strong> item left</span>
+        {/*Remove this if you don't implement routing*/}
+        <ul className="filters">
+          <li>
+            <a className="selected" href="#/">All</a>
+          </li>
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+          <li>
+            <a href="#/completed">Completed</a>
+          </li>
+        </ul>
+        {/*Hidden if no completed items are left ↓*/}
+        <button className="clear-completed">Clear completed</button>
+      </footer>
+    );
+  }
+
   render() {
     return (
       <div>
         <section className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <input className="new-todo" placeholder="What needs to be done?" autoFocus />
-          </header>
+          {this.renderHeader()}
+
           {/*This section should be hidden by default and shown when there are todos*/}
-          <section className="main">
-            <input className="toggle-all" type="checkbox" />
-            {/*<label for="toggle-all">Mark all as complete</label>*/}
-            <ul className="todo-list">
-              {/*These are here just to show the structure of the list items*/}
-              {/*List items should get the class `editing` when editing and `completed` when marked as completed*/}
-              <li className="completed">
-                <div className="view">
-                  <input className="toggle" type="checkbox" checked />
-                  <label>Taste JavaScript</label>
-                  <button className="destroy" />
-                </div>
-                <input className="edit" value="Create a TodoMVC template" />
-              </li>
-              <li>
-                <div className="view">
-                  <input className="toggle" type="checkbox" />
-                  <label>Buy a unicorn</label>
-                  <button className="destroy" />
-                </div>
-                <input className="edit" value="Rule the web" />
-              </li>
-            </ul>
-          </section>
+          {this.renderMain()}
+          
           {/*This footer should hidden by default and shown when there are todos*/}
-          <footer className="footer">
-            {/*This should be `0 items left` by default*/}
-            <span className="todo-count"><strong>0</strong> item left</span>
-            {/*Remove this if you don't implement routing*/}
-            <ul className="filters">
-              <li>
-                <a className="selected" href="#/">All</a>
-              </li>
-              <li>
-                <a href="#/active">Active</a>
-              </li>
-              <li>
-                <a href="#/completed">Completed</a>
-              </li>
-            </ul>
-            {/*Hidden if no completed items are left ↓*/}
-            <button className="clear-completed">Clear completed</button>
-          </footer>
+          {this.renderFooter()}
         </section>
-        <footer className="info">
-          <p>Double-click to edit a todo</p>
-          <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-        </footer>
       </div>
     );
   }
